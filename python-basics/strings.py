fdd49ada0b31b9f046443e222724e18ec0e7be0b
Rezist5 <dudikov.ivanchik@gmail.com>
2026-01-21 15:55:08 +0500

    Updated strings and variables

--- python-basics/strings.py
+++ python-basics/strings.py
@@ -1,3 +1,18 @@
 print("It's alright")
 print("He is called 'Johnny'")
 print('He is called "Johnny"')
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello, World!"
+print(a.upper())
+
+b = "Hello, World!"
+print(b[2:5])
--- python-basics/variables.py
+++ python-basics/variables.py
@@ -2,6 +2,29 @@ x = 5
 y = "John"
 print(x)
 print(y)
+
+
 print(type(x))
 print(type(y))
 
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = "Python is awesome"
+print(x)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+myfunc()
