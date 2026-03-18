646aab2e1bff8e58484d5f0228e668d5ced33a24
Rezist5 <dudikov.ivanchik@gmail.com>
2026-03-18 15:12:58 +0500

    Update prac 6

--- Practice6/directory_management/move_files.py
+++ Practice6/directory_management/move_files.py
@@ -1,16 +1,12 @@
 import os
 import shutil
 
-# перемещение
 shutil.move("a.txt", "test/a.txt")
 
-# копирование
 shutil.copy("a.txt", "copy_a.txt")
 shutil.copy2("a.txt", "copy2_a.txt")
 
-# переименование
 os.rename("copy_a.txt", "renamed.txt")
 
-# удаление
 os.remove("renamed.txt")
 shutil.rmtree("test")
